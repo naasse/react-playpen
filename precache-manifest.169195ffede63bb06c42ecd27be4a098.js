16bc43e13eacb2d7f7e0a4ad8fd118884c0a53c3
Nathan Asselstine <nate.asselstine@gmail.com>
2020-09-15 22:33:03 -0400

    Updates

---
+++ precache-manifest.169195ffede63bb06c42ecd27be4a098.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "467d689fe31260c0bec488e4b8aeafe4",
+    "url": "/react-playpen/index.html"
+  },
+  {
+    "revision": "50a7198cf1dc59b418d8",
+    "url": "/react-playpen/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b0b17fb94a27d8278cf1",
+    "url": "/react-playpen/static/js/2.7f2faf4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-playpen/static/js/2.7f2faf4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50a7198cf1dc59b418d8",
+    "url": "/react-playpen/static/js/main.d2b649d5.chunk.js"
+  },
+  {
+    "revision": "1a0de55bbc331279d790",
+    "url": "/react-playpen/static/js/runtime-main.9084d10e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-playpen/static/media/logo.5d5d9eef.svg"
+  }
+]);

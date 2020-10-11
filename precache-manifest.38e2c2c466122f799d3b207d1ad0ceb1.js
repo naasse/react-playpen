79599bcb4363a5ad9780e8e6ad25c085aad6cc17
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-11 01:01:52 +0000

    Deploy naasse/react-playpen to github.com/naasse/react-playpen.git:gh-pages

---
+++ precache-manifest.38e2c2c466122f799d3b207d1ad0ceb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4327e7c50c669a80eb31fb0bdf6ef5",
+    "url": "/react-playpen/index.html"
+  },
+  {
+    "revision": "d3e9c87b29523da53c6d",
+    "url": "/react-playpen/static/css/main.95dff03a.chunk.css"
+  },
+  {
+    "revision": "bc425b2088c97fedb413",
+    "url": "/react-playpen/static/js/2.c50d90e5.chunk.js"
+  },
+  {
+    "revision": "0bfb10e5d591fad7dc1e73daaaf7af7e",
+    "url": "/react-playpen/static/js/2.c50d90e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3e9c87b29523da53c6d",
+    "url": "/react-playpen/static/js/main.942ac2e8.chunk.js"
+  },
+  {
+    "revision": "2c0c5dccdc4c22dfe287",
+    "url": "/react-playpen/static/js/runtime-main.e2628013.js"
+  }
+]);
